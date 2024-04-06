69b75ec27224bf9008e945539221563c828e6d77
Mingyuan Li <jasperli@bitgo.com>
2024-04-06 12:00:35 -0400

    swap test

---
+++ tests/swap.ts
@@ -0,0 +1,22 @@
+import * as anchor from "@coral-xyz/anchor";
+import * as splToken from "@solana/spl-token";
+
+import { Program } from "@coral-xyz/anchor";
+import { LiquidityBootstrapFjord } from "../target/types/liquidity_bootstrap_fjord";
+import { assert } from "chai";
+import { SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+
+describe("swap", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace
+    .LiquidityBootstrapFjord as Program<LiquidityBootstrapFjord>;
+
+  it("swaps exact assets for shares", async () => {
+    
+  });
+});
