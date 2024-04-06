0cf6b61b9d36badef8ffb9797cf36e385cdd5e41
Dhruv <dhruv@tensor.so>
2024-04-06 10:10:01 -0400

    wrap up swap_assets_for_exact_shares

--- programs/liquidity-bootstrap-fjord/src/instructions/swap_assets_for_exact_shares.rs
+++ programs/liquidity-bootstrap-fjord/src/instructions/swap_assets_for_exact_shares.rs
@@ -3,6 +3,7 @@ use anchor_spl::token::{self, TokenAccount, Transfer, Mint};
 use crate::state::*;
 
 #[derive(Accounts)]
+#[instruction(referrer: Pubkey)]
 pub struct SwapAssetsForExactShares<'info> {
   #[account(mut)]
   pub depositor: Signer<'info>,
@@ -22,16 +23,28 @@ pub struct SwapAssetsForExactShares<'info> {
   #[account(
     init,
     seeds = [
-      b"pool".as_ref(),
-      &lbp_manager_info.to_account_info().key().to_bytes(),
-      &asset_mint.key().as_ref(),
-      &share_mint.key().as_ref()
+      b"user_stats".as_ref(),
+      &pool.key().as_ref(),
+      &depositor.key().as_ref(),
     ],
     payer = depositor,
-    space = 32 + 32 + 32 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 8 + 1 + (1 + 8 + 8 + 1),
+    space = 8 + 32 + 32 + 8 + 8 + 1,
     bump,
   )]
-  pub pool: Box<Account<'info, Pool>>,
+  pub buyer_stats: Box<Account<'info, UserStats>>,
+
+  #[account(
+    init,
+    seeds = [
+      b"user_stats".as_ref(),
+      &pool.key().as_ref(),
+      &referrer.key().as_ref(),
+    ],
+    payer = depositor,
+    space = 8 + 32 + 32 + 8 + 8 + 1,
+    bump,
+  )]
+  pub referrer_stats: Box<Account<'info, UserStats>>,
 }
 
 pub fn handler (
@@ -46,6 +59,8 @@ pub fn handler (
   // Get the pool and manager
   let pool = &mut ctx.accounts.pool;
   let manager = &mut ctx.accounts.lbp_manager;
+  let buyer_stats = &mut ctx.accounts.buyer_stats;
+  let referrer_stats = &mut ctx.accounts.referrer_stats;
 
   let assets: u64 = ctx.accounts.pool_assets_account.amount;
   let shares: u64 = ctx.accounts.pool_shares_account.amount;
@@ -88,9 +103,16 @@ pub fn handler (
   }
 
   pool.total_purchased = total_purchased_after;
+  buyer_stats.purchased += shares_out;
+
+  if (recipient != Pubkey::default() && manager.referrer_fee > 0) {
+    //             uint256 assetsReferred = assetsIn.mulWad(referrerFee());
+    let assets_referred: u64 = assets_in * manager.referrer_fee;
+    referred_stats.referred_amount += assets_referred;
+  }
 
 
-
+  // TODO: Emit an event here
 
   Ok(())
 }
